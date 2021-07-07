7af5db5980bec60af4c9c955090597262182aa6c
nerooc <tomasz_gajda@outlook.com>
2021-07-07 22:08:59 +0200

    bulma styling, basic comparison and conditional rendering

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
 })
 export class AppComponent {
-  title = 'typing-game';
+  title = "typing-game";
+
+  exampleString = "Lorem ipsum dolor sit amet";
+  inputString = "";
+  isWon = false;
+
+  updateInputString(value) {
+    this.inputString = value;
+  }
+
+  onInput(value) {
+    this.updateInputString(value);
+    this.isWon = this.inputString === this.exampleString;
+  }
 }
